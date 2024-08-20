12e350755cec3e21178fb267e55d5343567569c3
Lakshay Gulati <116188979+lakshay-gulati-07@users.noreply.github.com>
2024-08-21 00:56:34 +0530

    Update main.py

--- PongGame/main.py
+++ PongGame/main.py
@@ -2,6 +2,7 @@ import turtle
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 
 screen = turtle.Screen()
@@ -11,7 +12,9 @@ screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.listen()
 
+
 ball = Ball()
+score = Scoreboard()
 
 l_paddle = Paddle((-350,0))
 r_paddle = Paddle((350,0))
@@ -24,11 +27,26 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.ybounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() >320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.xbounce()
+
+    # Misses right Paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+        
+
+    
+    # Misses Left Paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 
 
