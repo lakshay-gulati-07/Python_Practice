fe1c06e9bf29dfbf940e5e6a3534203736a284d1
Lakshay Gulati <116188979+lakshay-gulati-07@users.noreply.github.com>
2024-08-21 00:56:05 +0530

    Update ball.py

--- PongGame/ball.py
+++ PongGame/ball.py
@@ -9,12 +9,22 @@ class Ball(Turtle):
         self.game_is_on = True
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
     
     def move(self):
         new_x = self.xcor()+ self.x_move
         new_y = self.ycor()+ self.y_move
         self.goto(new_x,new_y)
 
-    def bounce(self):
+    def ybounce(self):
         self.y_move *= -1
+
+    def xbounce(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+    
+    def reset_position(self):
+        self.goto(0,0)
+        self.xbounce()
+        self.move_speed = 0.1
         
