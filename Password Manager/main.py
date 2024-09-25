4a14224e39306a4b6f401f1c2e2a2e5d27ef620f
Lakshay Gulati <116188979+lakshay-gulati-07@users.noreply.github.com>
2024-09-26 00:25:27 +0530

    Update main.py

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 window = Tk()
 
 # window Creation
@@ -36,11 +38,37 @@ pass_label = Label(text="Password:",bg="black",font=("times new roman",24,"norma
 pass_label.grid(row=3,column=0)
 
 # Password Entry
-pass_entry = Entry(width=20)
+pass_entry = Entry(width=25)
 pass_entry.grid(row=3,column=1)
 
+
+# password Generate
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for item in range(nr_letters)]
+    password_numbers = [random.choice(numbers) for number in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for symbol in range(nr_symbols)]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    print(f"Your password is: {password}")
+    pass_entry.insert(0,f"{password}")
+    pyperclip.copy(password)
+
+
 # Generate Passwod Button
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password",command=generate_password)
 generate_button.grid(row=3,column=2)
 
 
@@ -60,16 +88,11 @@ def save():
 
 
 
+
+
 # add Button
 add_button = Button(text="Add",width=36,command=save)
 add_button.grid(row=4,column=1,columnspan=2)
-
-
-
-
-
-
-
 
 
 window.mainloop()
